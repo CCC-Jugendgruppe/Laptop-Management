9511ee1714014b2dc7f6abec319e0fc6334b3f4e
hyperbel <hyperbel@users.noreply.github.com>
2023-10-29 19:38:27 +0100

    preseeds & initrds are weird

--- preseed/update-preseed.sh
+++ preseed/update-preseed.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 gunzip -v initrd.gz
 cp -v initrd initrd.orig
-wget https://raw.githubusercontent.com/CCC-Jugendgruppe/Laptop-Management/main/iso/custom-preseed.cfg -O preseed.cfg
+wget https://raw.githubusercontent.com/CCC-Jugendgruppe/Laptop-Management/main/iso/custom-preseed.cfg -O custom-preseed.cfg
+cp custom-preseed.cfg preseed.cfg
 echo preseed.cfg | cpio -H newc -o -A -F initrd
 gzip -v initrd
