99946928553df203cfb8e832a5937bb9d08c6d7b
hyperbel <hyperbel@users.noreply.github.com>
2023-10-29 19:34:11 +0100

    preseed instead of iso

---
+++ preseed/README.md
@@ -0,0 +1,3 @@
+# Custom Preseed
+
+Run the `update-preseed.sh` script in the same dir as the `initrd.gz` file. This is probably `/srv/tftp/debian-installer/amd64`.
---
+++ preseed/update-preseed.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+gunzip -v initrd.gz
+cp -v initrd initrd.orig
+wget https://raw.githubusercontent.com/CCC-Jugendgruppe/Laptop-Management/main/iso/custom-preseed.cfg -O preseed.cfg
+echo preseed.cfg | cpio -H newc -o -A -F initrd
+gzip -v initrd
