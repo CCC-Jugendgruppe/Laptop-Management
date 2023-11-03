f8b1b6aefc94dab5bb3562902b8b17678432c413
hyperbel <hyperbel@users.noreply.github.com>
2023-11-03 21:35:52 +0100

    update preseed in update preseed script

--- preseed/update-preseed.sh
+++ preseed/update-preseed.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 gunzip -v initrd.gz
 cp -v initrd initrd.orig
-wget https://raw.githubusercontent.com/CCC-Jugendgruppe/Laptop-Management/main/iso/custom-preseed.cfg -O custom-preseed.cfg
-cp custom-preseed.cfg preseed.cfg
+wget https://raw.githubusercontent.com/CCC-Jugendgruppe/Laptop-Management/main/iso/preseed.cfg -O preseed.cfg
 echo preseed.cfg | cpio -H newc -o -A -F initrd
 gzip -v initrd
