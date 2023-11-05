ad97b4db73609b59793306a1e1ee998c378f2251
LeSnake <dev.lesnake@posteo.de>
2023-11-05 16:40:24 +0100

    show available tags when no provided

--- Management/ansible-run.sh
+++ Management/ansible-run.sh
@@ -7,8 +7,17 @@
 EXTRA=''
 
 [[ ! -z $BECOME ]] && EXTRA='--ask-become-pass'
-echo $EXTRA
+
+if [ -z "$1" ]; then
+	echo "Avaiable Tags:"
+	# get available tags, ignore leading info (only get last line)
+	TAGS=$(ansible-playbook $FILE --list-tags | tail -n 1 ) 
+	# trim ] from the end
+	TAGS=${TAGS::-1}
+	# trim tags from start until(including) the first [
+	echo ${TAGS#*[}
+    exit
+fi
 
 # run command to execute playbook
-
 ansible-playbook $FILE -i $HOSTS $EXTRA --tags "$@" 
