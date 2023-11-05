fec10006acca150eecb188719e4d61e157b913ab
LeSnake <dev.lesnake@posteo.de>
2023-11-05 22:19:56 +0100

    added set background task

---
+++ Management/roles/wallpaper/tasks/set_wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/env sh
+
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///usr/share/wallpapers/ccc-jugendgruppe-goe.jpg")}'
